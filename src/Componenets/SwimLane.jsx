78568869f6becdfa7bff2621eb567058f80f00ef
peadar-kenny <kennyp9@tcd.ie>
2022-02-28 17:53:18 +0000

    Maintained Swimlane component

--- src/Componenets/SwimLane.jsx
+++ src/Componenets/SwimLane.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 
 const SwimLane = ({}) => {
-    return ()
+    return (
+        <>
+        <Typography>Hello World</Typography>
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'flex-start',
+            flexDirection: 'column',
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+          }}
+        > 
+
+        
+        </Box>
+        </>
+    )
 }
+export default SwimLane;
