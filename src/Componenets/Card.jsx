16c3fcb967956b52b424e5425089220e67dbc07a
loconno5 <loconno5@tcd.ie>
2022-02-28 17:04:57 +0000

    adding card component

--- src/Componenets/Card.jsx
+++ src/Componenets/Card.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Box } from '@mui/system';
 
 const Card = ({}) => {
     return (
-
-    );
+<div style={{}}>
+    <Card>
+    <Box sx={{ p: 2, display: 'flex' }}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Card Title
+        </Typography>
+        <Typography
+          style={{
+            marginBottom: 12,
+          }}
+          color="textSecondary"
+        >Some text description
+        </Typography>
+      </CardContent>
+      </Box>
+    </Card>
+  </div>
+    )
+   
 }
+export default Card;
