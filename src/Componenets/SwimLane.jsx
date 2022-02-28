be399b7ab162ba852c580a2b42af7dbad15fdf34
peadar-kenny <kennyp9@tcd.ie>
2022-02-28 17:56:10 +0000

    added formatting

--- src/Componenets/SwimLane.jsx
+++ src/Componenets/SwimLane.jsx
@@ -3,7 +3,15 @@ import { Box, Typography } from '@material-ui/core';
 
 const SwimLane = ({}) => {
     return (
-        <>
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'flex-start',
+            flexDirection: 'column',
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+          }}
+        >
         <Typography>Hello World</Typography>
         <Box sx={{
             display: 'flex',
@@ -15,9 +23,9 @@ const SwimLane = ({}) => {
           }}
         > 
 
-        
+        <Typography>Hello World</Typography>
         </Box>
-        </>
+        </Box>
     )
 }
 export default SwimLane;
