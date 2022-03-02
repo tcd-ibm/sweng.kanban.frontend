778f6b5d1d74441d82a907da88b0d4c5cde403e4
peadar-kenny <kennyp9@tcd.ie>
2022-03-02 16:11:14 +0000

    added initial demonstration implementation

--- src/Componenets/SwimLane.jsx
+++ src/Componenets/SwimLane.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import KBCard from './Card';
 
-const SwimLane = ({}) => {
-    return ()
+const SwimLane = ({swimLaneData, swimLaneTitle}) => {
+    return (
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'flex-start',
+            flexDirection: 'column',
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+          }}
+        >
+        <Typography>{swimLaneTitle}</Typography>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            p: 1,
+            m: 1,
+            borderRadius: 1,
+          }}
+        > 
+        {swimLaneData?.map(card =>
+            <KBCard title={card.title} desc={card.desc} />
+            )}
+        </Box>
+        </Box>
+    )
 }
+export default SwimLane;
