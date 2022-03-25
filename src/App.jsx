9ee747c8fd517305b9f1c200e4aa7df40e61232f
loconno5 <loconno5@tcd.ie>
2022-03-25 18:14:24 +0000

    add get request

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import SwimLane from './Componenets/SwimLane';
 import { Box } from '@material-ui/core';
+import axios from 'axios';
+import React from "react";
+import { useEffect } from 'react';
+
+
 
 const data = [{title:"A title" , desc: "1234"}, {title: "Placeholder task", desc: "4321"}, {title: "Group 31", desc: "100/100"}, {title: "IBM open-audio-software", desc: "Never Forget"} ];
 const swimlanes=[
                   {cards: [data[0], data[1], data[2]], title:"To Do" },
                   {cards: [data[3], data[2], data[0]], title:"In Progress"},  
                   {cards: [data[0], data[1], data[2]], title:"Done" }
-                ]
+                ] 
+
 
 function App() {
+
+  useEffect(() => {
+    axios.get('http://kanbanbackend-petrukhp-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/listAllKanbanBoards')
+    .then(res => console.log(res.data[0]))
+  },[])
   return (
+
     
     <Box sx={{
       display: 'flex',
@@ -27,7 +39,7 @@ function App() {
       
     
     
-    </Box>
+    </Box> 
   );
 }
 
